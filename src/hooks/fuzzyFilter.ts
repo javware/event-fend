10c3d225b349598ca739de1c62074bc8ba21fed4
javware <xaviercito052016@gmail.com>
2024-10-30 20:36:23 -0500

    update files fuzzy

--- src/hooks/fuzzyFilter.ts
+++ src/hooks/fuzzyFilter.ts
@@ -1,8 +1,8 @@
 import { rankItem } from "@tanstack/match-sorter-utils";
 
-export const fuzzyFilter = (row: any, columnId: string, value: string, addMeta: (meta: { itemRank: any }) => void) => {
+export const fuzzyFilter = (row: any, value: string, addMeta: (meta: { itemRank: any }) => void) => {
     const combinedValue = Object.values(row.original)
-        .filter(val => typeof val === 'string' || typeof val === 'number') // Filtrar solo valores tipo string y número
+        .filter(val => typeof val === 'string' || typeof val === 'number')
         .join(' ')
         .toLowerCase();
 
